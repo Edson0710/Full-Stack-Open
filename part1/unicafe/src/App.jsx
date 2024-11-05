26acb64d8cd79d81c36b13ff7d91f21668d414f0
Edson Daniel Navarro Brizuela <edsonnavarro0710@gmail.com>
2024-11-04 20:32:29 -0600

    Ejercicio 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,12 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({ text, value }) => (
+  <p>
+    {text} {value}
+  </p>
+)
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad
   const average = (good - bad) / total
@@ -13,12 +19,12 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive}%</p>
+      <StatisticLine text='good' value={good} />
+      <StatisticLine text='neutral' value={neutral} />
+      <StatisticLine text='bad' value={bad} />
+      <StatisticLine text='all' value={total} />
+      <StatisticLine text='average' value={average || 0} />
+      <StatisticLine text='positive' value={positive || 0} />
     </div>
   )
 }
